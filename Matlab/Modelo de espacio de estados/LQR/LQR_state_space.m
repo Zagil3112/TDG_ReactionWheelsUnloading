4b114f32671a9b795519981b72f599935488873a
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2023-08-01 17:44:34 -0500

    lqr simple

--- Matlab/Modelo de espacio de estados/LQR/LQR_state_space.m
+++ Matlab/Modelo de espacio de estados/LQR/LQR_state_space.m
@@ -124,81 +124,81 @@ x0(2) =0;
 x0(3) = 0;
 x0(6) = -sin((pi/4)/2);
 
-%%
-% simulation = sim('SC_LQR_model');
 
-%extract data
-% t = simulation.sim_X.time;
-% 
-% x1 = simulation.sim_X.signals.values(:,1);
-% x2 = simulation.sim_X.signals.values(:,2);
-% x3 = simulation.sim_X.signals.values(:,3);
-% 
-% x6 = simulation.sim_X.signals.values(:,6);
-% 
-% u1 = simulation.sim_U.signals.values(:,1);
-% u2 = simulation.sim_U.signals.values(:,2);
-% u3 = simulation.sim_U.signals.values(:,3);
-% 
-% % RW torque
-% RW_torque = simulation.RW_torque.signals.values();
-% 
-% 
-% %plot
-% figure(1)
-% subplot(3,1,1)
-% plot(t, x1, 'LineWidth', 3)
-% title(['Scenario ',num2str(scenario)])
-% grid on
-% legend('x_1')
-% 
-% subplot(3,1,2)
-% plot(t, x2, 'LineWidth', 3)
-% grid on
-% legend('x_2')
-% 
-% subplot(3,1,3)
-% plot(t, x3, 'LineWidth', 3)
-% grid on
-% legend('x_3')
-% 
-% %% Torques
-% % figure(2)
-% % subplot(3,1,1)
-% % plot(t, u1, 'LineWidth', 3)
-% % title(['Scenario ',num2str(scenario)])
-% % grid on
-% % legend('u_1')
-% % 
-% % subplot(3,1,2)
-% % plot(t, u2, 'LineWidth', 3)
-% % grid on
-% % legend('u_2')
-% % 
-% % subplot(3,1,3)
-% % plot(t, u3, 'LineWidth', 3)
-% % grid on
-% % legend('u_3')
-% 
-% %%
-% % figure(3)
-% % plot(t, x6, 'LineWidth', 3)
-% % title(['Scenario ',num2str(scenario)])
-% % grid on
-% % legend('x_6')
-% 
-% figure(4)
-% title("RW torque")
-% subplot(3,1,1)
-% plot(t, RW_torque(:,1), 'LineWidth', 3)
-% legend('TX')
-% 
-% 
-% subplot(3,1,2)
-% plot(t, RW_torque(:,2), 'LineWidth', 3)
-% legend('TY')
-% 
-% 
-% subplot(3,1,3)
-% plot(t, RW_torque(:,3), 'LineWidth', 3)
-% legend('TZ')
+simulation = sim('SC_LQR_model');
+
+% extract data
+t = simulation.sim_X.time;
+
+x1 = simulation.sim_X.signals.values(:,1);
+x2 = simulation.sim_X.signals.values(:,2);
+x3 = simulation.sim_X.signals.values(:,3);
+
+x6 = simulation.sim_X.signals.values(:,6);
+
+u1 = simulation.sim_U.signals.values(:,1);
+u2 = simulation.sim_U.signals.values(:,2);
+u3 = simulation.sim_U.signals.values(:,3);
+
+% RW torque
+RW_torque = simulation.RW_torque.signals.values();
+
+
+% plot
+figure(1)
+subplot(3,1,1)
+plot(t, x1, 'LineWidth', 3)
+title(['Scenario ',num2str(scenario)])
+grid on
+legend('x_1')
+
+subplot(3,1,2)
+plot(t, x2, 'LineWidth', 3)
+grid on
+legend('x_2')
+
+subplot(3,1,3)
+plot(t, x3, 'LineWidth', 3)
+grid on
+legend('x_3')
+
+% Torques
+figure(2)
+subplot(3,1,1)
+plot(t, u1, 'LineWidth', 3)
+title(['Scenario ',num2str(scenario)])
+grid on
+legend('u_1')
+
+subplot(3,1,2)
+plot(t, u2, 'LineWidth', 3)
+grid on
+legend('u_2')
+
+subplot(3,1,3)
+plot(t, u3, 'LineWidth', 3)
+grid on
+legend('u_3')
+
+%
+figure(3)
+plot(t, x6, 'LineWidth', 3)
+title(['Scenario ',num2str(scenario)])
+grid on
+legend('x_6')
+
+figure(4)
+title("RW torque")
+subplot(3,1,1)
+plot(t, RW_torque(:,1), 'LineWidth', 3)
+legend('TX')
+
+
+subplot(3,1,2)
+plot(t, RW_torque(:,2), 'LineWidth', 3)
+legend('TY')
+
+
+subplot(3,1,3)
+plot(t, RW_torque(:,3), 'LineWidth', 3)
+legend('TZ')
