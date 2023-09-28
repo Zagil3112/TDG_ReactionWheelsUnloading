7875e2db6b9991cc4dc6c4c106a02abeb546a843
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2023-09-28 18:24:00 -0500

    feat script 2 run sims

--- Matlab/RunSimulations.m
+++ Matlab/RunSimulations.m
@@ -1,33 +1,59 @@
 %% Params
 t_sim = 20000;
-t = LQR_PID_controller_sim.Ref_Error.time;
+
 %% Paths
 LQR_PID_controller = ".\Controladores\LQR\NonLinear\LQR_bloques_SS_LQR_cascada_PID_controller_NonLinear";
 PID_deltaH_controller = ".\Controladores\PID\PID_mgtXY_loadingTest";
+LQR_I_controller = ".\Controladores\LQR\NonLinear\LQR_bloques_SS_LQR_cascada_I_controller_NonLinear";
+LQR_deltaH_controller = ".\Controladores\LQR\NonLinear\LQR_bloques_SS_LQR_Delta_H_NonLinear";
 
-% %% LQR Cascade PID
-% LQR_PID_controller_sim = sim(LQR_PID_controller);
-% 
-% %Error de Referencia
-% LQR_PID_YawError = LQR_PID_controller_sim.Ref_Error.signals.values(:,1);
-% 
-% figure(1)
-% plot(t, LQR_PID_YawError, 'LineWidth', 3)
-% title('LQR PID Yaw Error [rad]')
-% grid on
 
-%% PID delta H 
-PID_deltaH_controller_sim = sim(PID_deltaH_controller);
-t2 = PID_deltaH_controller_sim.Ref_Error.time;
-
+%% LQR Cascade PID
+LQR_PID_controller_sim = sim(LQR_PID_controller);
+t1 = LQR_PID_controller_sim.Ref_Error.time;
 %Error de Referencia
-PID_deltaH_YawError = PID_deltaH_controller_sim.Ref_Error.signals.values(:,:,1:50);
+LQR_PID_YawError = LQR_PID_controller_sim.Ref_Error.signals.values(:,1);
 
-figure(2)
-plot(t2, PID_deltaH_YawError, 'LineWidth', 3)
-title('PID Yaw Error [rad]')
+figure(1)
+plot(t1, LQR_PID_YawError, 'LineWidth', 3)
+title('LQR PID Yaw Error [rad]')
 grid on
 
+%% PID delta H 
+% PID_deltaH_controller_sim = sim(PID_deltaH_controller);
+% t2 = PID_deltaH_controller_sim.Ref_Error.time;
+% 
+% %Error de Referencia
+% PID_deltaH_YawError = PID_deltaH_controller_sim.Ref_Error.signals.values(:,:,1:50);
+% 
+% figure(2)
+% plot(t2, PID_deltaH_YawError, 'LineWidth', 3)
+% title('PID Yaw Error [rad]')
+% grid on
+
+%% LQR Cascade I
+LQR_I_controller_sim = sim(LQR_I_controller);
+t3 = LQR_I_controller_sim.Ref_Error.time;
+%Error de Referencia
+LQR_I_YawError = LQR_I_controller_sim.Ref_Error.signals.values(:,1);
+
+figure(3)
+plot(t3, LQR_I_YawError, 'LineWidth', 3)
+title('LQR I Yaw Error [rad]')
+grid on
+
+%% LQR Delta H 
+LQR_deltaH_controller_sim = sim(LQR_deltaH_controller);
+t4 = LQR_deltaH_controller_sim.Ref_Error.time;
+%Error de Referencia
+LQR_deltaH_YawError = LQR_deltaH_controller_sim.Ref_Error.signals.values(:,1);
+
+figure(4)
+plot(t4, LQR_deltaH_YawError, 'LineWidth', 3)
+title('LQR Delta H Yaw Error [rad]')
+grid on
+
+%%
 % RW_torqueX = simulation.RW_torque.signals.values(:,1);
 % RW_torqueZ = simulation.RW_torque.signals.values(:,3);
 % 
