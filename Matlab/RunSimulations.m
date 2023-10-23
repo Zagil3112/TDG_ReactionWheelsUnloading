8a6d75b8f738e61838fa9052aa0f6a385761aef9
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2023-10-23 00:31:47 -0500

    update graphs

--- Matlab/RunSimulations.m
+++ Matlab/RunSimulations.m
@@ -1,12 +1,15 @@
 %% Params
-t_sim = 10000;
+t_sim = 60;
+init_step=5;
+end_step=30;
+
 global plot_flag orbit_path profile;
 
 plot_flag = false;
  
-orbit_path="orbit_zero";
+orbit_path="orbit_polar";
 profile = "Step";
-magnetic_field= B_mag0;
+magnetic_field= B_mag90;
 
 %B_mag51,B_mag90,B_mag0
 
@@ -28,20 +31,39 @@ LQR_I_controller= sim(LQR_I_path);
 LQR_deltaH_controller= sim(LQR_deltaH_path);
 
 %% LQR Cascade PID
-[t1,YawError1,SC_omega1,RW_h1,RW_torques1,Mgt_torques1] = GetControllerPlots(LQR_PID_controller,"LQR PID controller");
+[t1,YawError1,SC_omega1,RW_h1,RW_torques1,Mgt_torques1,Yaw1] = GetControllerPlots(LQR_PID_controller,"LQR PID controller");
 %% PID delta H 
-[t2,YawError2,SC_omega2,RW_h2,RW_torques2,Mgt_torques2] = GetControllerPlots(PID_deltaH_controller,"PID delta H controller");
+[t2,YawError2,SC_omega2,RW_h2,RW_torques2,Mgt_torques2,Yaw2] = GetControllerPlots(PID_deltaH_controller,"PID delta H controller");
 %% LQR Cascade I
-[t3,YawError3,SC_omega3,RW_h3,RW_torques3,Mgt_torques3,Yaw_ref3] = GetControllerPlots2(LQR_I_controller,"LQR I controller");
+[t3,YawError3,SC_omega3,RW_h3,RW_torques3,Mgt_torques3,Yaw3] = GetControllerPlots(LQR_I_controller,"LQR I controller");
 %% LQR Delta H 
-[t4,YawError4,SC_omega4,RW_h4,RW_torques4,Mgt_torques4] = GetControllerPlots(LQR_deltaH_controller,"LQR delta H controller");
+[t4,YawError4,SC_omega4,RW_h4,RW_torques4,Mgt_torques4,Yaw4] = GetControllerPlots(LQR_deltaH_controller,"LQR delta H controller");
 %% Plots
 
+% Graficar omega, error de yaw y momoneto angular para los 4 controladores
+figure()
+subplot(1,3,1)
+groupPlots(t1, Yaw1, t2, Yaw2, t3, Yaw3, t4, Yaw4, "Yaw [deg]","yaw");
+
+
+
+subplot(1,3,2)
 groupPlots(t1, SC_omega1, t2, SC_omega2, t3, SC_omega3, t4, SC_omega4, "SC Omega [rad/s]","sc_omega");
-groupPlots(t1, YawError1, t2, YawError2, t3, YawError3, t4, YawError4, "Yaw Error [rad]","yaw_error");
+
+
+subplot(1,3,3)
 groupPlots(t1, RW_h1, t2, RW_h2, t3, RW_h3, t4, RW_h4, "RW Momento angular [Nms]","RW_h");
-groupPlots(t1, RW_torques1, t2, RW_torques2, t3, RW_torques3, t4, RW_torques4, "RW Z axis torques [Nm]","RW_torques");
-groupPlots(t1, Mgt_torques1, t2, Mgt_torques2, t3, Mgt_torques3, t4, Mgt_torques4, "Mgt Z axis torques [Nm]","Mgt_torques");
+saveas(gcf,"./Resultados_imagenes/"+profile+'/'+orbit_path+'/'+'yaw_omega_h.fig')
+    
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% groupPlots(t1, SC_omega1, t2, SC_omega2, t3, SC_omega3, t4, SC_omega4, "SC Omega [rad/s]","sc_omega");
+% groupPlots(t1, YawError1, t2, YawError2, t3, YawError3, t4, YawError4, "Yaw Error [rad]","yaw_error");
+% groupPlots(t1, RW_h1, t2, RW_h2, t3, RW_h3, t4, RW_h4, "RW Momento angular [Nms]","RW_h");
+% groupPlots(t1, RW_torques1, t2, RW_torques2, t3, RW_torques3, t4, RW_torques4, "RW Z axis torques [Nm]","RW_torques");
+% groupPlots(t1, Mgt_torques1, t2, Mgt_torques2, t3, Mgt_torques3, t4, Mgt_torques4, "Mgt Z axis torques [Nm]","Mgt_torques");
 
 % plots3by1(t1,YawError1,SC_omega1,RW_h1)
 % plots3by1(t2,YawError2,SC_omega2,RW_h2)
@@ -49,7 +71,7 @@ groupPlots(t1, Mgt_torques1, t2, Mgt_torques2, t3, Mgt_torques3, t4, Mgt_torques
 % plots3by1(t4,YawError4,SC_omega4,RW_h4)
 
 %%
-plots1by3(t3,Yaw_ref3,RW_torques3,RW_h3)
+% plots1by3(t3,Yaw_ref3,RW_torques3,RW_h3)
 
 
 
@@ -74,23 +96,21 @@ writetable(index_table,"./Resultados_imagenes/"+profile+'/'+orbit_path+'/'+orbit
 
 %% Functions
 
-function plots3by1(t,param1,param2,param3)
+function plots3by1_allControllers()
     figure()
     subplot(1,3,1)
-    plot(t,param1,'LineWidth', 3);
-    grid on
-    title('Error Yaw [rad]')
+    groupPlots(t1, YawError1, t2, YawError2, t3, YawError3, t4, YawError4, "Yaw Error [rad]","yaw_error");
     
+   
+   
     subplot(1,3,2)
-    plot(t,param2,'LineWidth', 3);
-    grid on
-    title('SC omega [rad/s]')
+    groupPlots(t1, SC_omega1, t2, SC_omega2, t3, SC_omega3, t4, SC_omega4, "SC Omega [rad/s]","sc_omega");
+    
     
     subplot(1,3,3)
-    plot(t,param3,'LineWidth', 3);
-    grid on
-    title('RW Momento Angular [Nms]')
-
+    groupPlots(t1, RW_h1, t2, RW_h2, t3, RW_h3, t4, RW_h4, "RW Momento angular [Nms]","RW_h");
+    saveas(gcf,"./Resultados_imagenes/"+profile+'/'+orbit_path+'/'+fileName+'_yaw_omega_h.fig')
+    
 end
 
 function plots1by3(t,param1,param2,param3)
@@ -129,7 +149,7 @@ end
 
 function groupPlots(t1, param1, t2, param2, t3, param3, t4, param4,PlotTitle,fileName)
     global orbit_path profile
-    figure()
+    % figure()
     
     plot(t1,param1,'LineWidth', 1);
     hold on
@@ -146,6 +166,8 @@ function groupPlots(t1, param1, t2, param2, t3, param3, t4, param4,PlotTitle,fil
     legend("LQR cascade PID","PID delta H","LQR cascade I","LQR delta H",Location="best")
     
     title(PlotTitle)
+    ylabel(PlotTitle)
+    xlabel("tiempo [s]")
     grid on
     hold off
 
@@ -156,11 +178,11 @@ function groupPlots(t1, param1, t2, param2, t3, param3, t4, param4,PlotTitle,fil
     % matlab.graphics.exportsetup.Height = '10cm';
     % matlab.graphics.exportsetup.Resolution = '600'; 
 
-    saveas(gcf,"./Resultados_imagenes/"+profile+'/'+orbit_path+'/'+fileName+'.png')
-    saveas(gcf,"./Resultados_imagenes/"+profile+'/'+orbit_path+'/'+fileName+'.fig')
+    % saveas(gcf,"./Resultados_imagenes/"+profile+'/'+orbit_path+'/'+fileName+'.png')
+    % saveas(gcf,"./Resultados_imagenes/"+profile+'/'+orbit_path+'/'+fileName+'.fig')
 end
 
-function [t,YawError,SC_omega,RW_h,RW_torques,Mgt_torques] = GetControllerPlots(ControllerObject,ControllerName)
+function [t,YawError,SC_omega,RW_h,RW_torques,Mgt_torques,Yaw] = GetControllerPlots(ControllerObject,ControllerName)
     global plot_flag
     t = ControllerObject.Ref_Error.time;
     
@@ -172,6 +194,11 @@ function [t,YawError,SC_omega,RW_h,RW_torques,Mgt_torques] = GetControllerPlots(
         title(ControllerName+' Yaw Error [rad]')
         grid on
     end
+    
+    %Yaw
+    Yaw = ControllerObject.Yaw.signals.values(:,1);
+
+
     % RW Torques Z 
     RW_torques = ControllerObject.RW_torque.signals.values(:,3);
      if (plot_flag)
